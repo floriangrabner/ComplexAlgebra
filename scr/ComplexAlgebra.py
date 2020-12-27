7185eca0fa8185284861dec94ff3ab8a1390dadb
floriangrabner <68219155+floriangrabner@users.noreply.github.com>
2020-12-27 22:57:04 +0100

    Add files via upload

--- scr/ComplexAlgebra.py
+++ scr/ComplexAlgebra.py
@@ -8,6 +8,17 @@ class ComplexNumber:
         self.imaginary_exponent = imaginary_exponent
         self.complex = (self.real, self.imaginary)
 
+    def simplify(self):
+        convert_table = {
+            0: (self.real + self.imaginary, 0),
+            1: (self.real, self.imaginary),
+            2: (self.real - self.imaginary, 0),
+            3: (self.real, - self.imaginary)}
+
+        index = self.imaginary_exponent % 4
+        self.real = convert_table[index][0]
+        self.imaginary = convert_table[index][1]
+
     def __str__(self):
         if self.imaginary < 0:
             return '(%d%dj)' % (self.real, self.imaginary)
@@ -16,57 +27,28 @@ class ComplexNumber:
             return '(%d+%dj)' % (self.real, self.imaginary)
 
 
-def add(x: ComplexNumber, y: ComplexNumber) -> ComplexNumber:
+def addition(x: ComplexNumber, y: ComplexNumber) -> ComplexNumber:
     real_sum = x.real + y.real
     imaginary_sum = x.imaginary + y.imaginary
 
     return ComplexNumber(real_sum, imaginary_sum)
 
 
-def simplify(x: ComplexNumber) -> None:
-    convert_table = {
-        0: (x.real + x.imaginary, 0),
-        1: (x.real, x.imaginary),
-        2: (x.real - x.imaginary, 0),
-        3: (x.real, -x.imaginary)}
+def subtraction(x: ComplexNumber, y: ComplexNumber) -> ComplexNumber:
+    real_sum = x.real - y.real
+    imaginary_sum = x.imaginary + y.imaginary
 
-    if x.imaginary_exponent == 0:
-        x.real = convert_table[0][0]
-
-    elif x.imaginary_exponent == 2:
-        x.real = convert_table[2][0]
-        x.imaginary = convert_table[2][1]
-
-    elif x.imaginary_exponent == 3:
-        x.imaginary = table[3][1]
-
-    elif x.imaginary_exponent > 3:
-        index = x.imaginary_exponent % 4
-        x.real = convert_table[index][0]
-        x.imaginary = convert_table[index][1]
+    return ComplexNumber(real_sum, imaginary_sum)
 
 
 def multiplication(x: ComplexNumber, y: ComplexNumber) -> ComplexNumber:
+    z1 = ComplexNumber(x.real * y.real, x.real * y.imaginary)
+    z2 = ComplexNumber(0, x.imaginary * y.real)
+    z3 = ComplexNumber(0, x.imaginary * y.imaginary, x.imaginary_exponent + y.imaginary_exponent)
+    z3.simplify()
+
+    return addition(addition(z1, z2), z3)
+
+
+def division(x: ComplexNumber, y: ComplexNumber) -> ComplexNumber:
     pass
-
-
-
-
-class ImaginaryNumber:
-    def __init__(self, coefficient: int = 1, power: int = 1):
-        self.j = {'sign': '+' if coefficient >= 0 else '-', 'coefficient': coefficient, 'imaginary_power': power}
-
-    def __str__(self):
-        if self.j['imaginary_power'] == 1:
-            return '%dj' % self.j['coefficient']
-
-        elif self.j['imaginary_power'] == 0:
-            return '%d' % (self.j['coefficient'])
-
-        elif self.j['coefficient'] == 0:
-            return '0'
-
-        else:
-            return '%dj^%d' % (self.j['coefficient'], self.j['imaginary_power'])
-
-
